6d3f24818b02146d1d0dc2c2dbde0b835d0667ed
unknown <juan.bernal05@usa.edu.co>
2024-09-02 18:20:24 -0500

    Token update for browsers

--- ProyectoVapes-backend/src/main/java/com/project/vapes/controller/UserController.java
+++ ProyectoVapes-backend/src/main/java/com/project/vapes/controller/UserController.java
@@ -11,7 +11,6 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-// localhost:8080/userController/user
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -56,7 +55,7 @@ public class UserController {
             throw new UserNotFoundException(id);
         }
         userRepository.deleteById(id);
-        return "User with id " + id + " has been deleted success.";
+        return "User with id " + id + " has been deleted successfully.";
 
     }
 }
